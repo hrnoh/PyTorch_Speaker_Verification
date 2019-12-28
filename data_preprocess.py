33d797964cc3a44fcbef54201b11b6372550aff4
hrnoh <hr_noh@korea.ac.kr>
2019-12-29 00:54:29 +0900

    add save_spectrogram_voxceleb func

--- data_preprocess.py
+++ data_preprocess.py
@@ -8,7 +8,7 @@ import numpy as np
 from hparam import hparam as hp
 
 # downloaded dataset path
-audio_path = glob.glob(os.path.dirname(hp.unprocessed_data))                                        
+audio_path = glob.glob(hp.unprocessed_data)
 
 def save_spectrogram_tisv():
     """ Full preprocess of text independent utterance. The log-mel-spectrogram is saved as numpy file.
@@ -23,7 +23,7 @@ def save_spectrogram_tisv():
     utter_min_len = (hp.data.tisv_frame * hp.data.hop + hp.data.window) * hp.data.sr    # lower bound of utterance length
     total_speaker_num = len(audio_path)
     train_speaker_num= (total_speaker_num//10)*9            # split total data 90% train and 10% test
-    print("total speaker number : %d"%total_speaker_num)
+    print("total speaker number : %d" % total_speaker_num)
     print("train : %d, test : %d"%(train_speaker_num, total_speaker_num-train_speaker_num))
     for i, folder in enumerate(audio_path):
         print("%dth speaker processing..."%i)
@@ -51,6 +51,41 @@ def save_spectrogram_tisv():
         else:
             np.save(os.path.join(hp.data.test_path, "speaker%d.npy"%(i-train_speaker_num)), utterances_spec)
 
+def save_spectrogram_voxceleb():
+    print("start text independent utterance feature extraction")
+    os.makedirs(hp.data.train_path, exist_ok=True)   # make folder to save train file
+    os.makedirs(hp.data.test_path, exist_ok=True)    # make folder to save test file
+
+    utter_min_len = (hp.data.tisv_frame * hp.data.hop + hp.data.window) * hp.data.sr  # lower bound of utterance length
+    total_speaker_num = len(audio_path)
+    train_speaker_num = (total_speaker_num // 10) * 9  # split total data 90% train and 10% test
+    print("total speaker number : %d" % total_speaker_num)
+    print("train : %d, test : %d" % (train_speaker_num, total_speaker_num - train_speaker_num))
+
+    for i, spk in enumerate(audio_path):
+        print("%dth speaker processing..." % i)
+        utterances_spec = []
+        wavs = glob.glob(os.path.join(spk, "**/*.wav"))
+        for wav in wavs:
+            utter, sr = librosa.core.load(wav, sr=hp.data.sr)
+            intervals = librosa.effects.split(utter, top_db=30)
+            for interval in intervals:
+                if (interval[1] - interval[0]) > utter_min_len:
+                    utter_part = utter[interval[0]:interval[1]]
+                    S = librosa.core.stft(y=utter_part, n_fft=hp.data.nfft,
+                                          win_length=int(hp.data.window * sr), hop_length=int(hp.data.hop * sr))
+                    S = np.abs(S) ** 2
+                    mel_basis = librosa.filters.mel(sr=hp.data.sr, n_fft=hp.data.nfft, n_mels=hp.data.nmels)
+                    S = np.log10(np.dot(mel_basis, S) + 1e-6)  # log mel spectrogram of utterances
+                    utterances_spec.append(S[:, :hp.data.tisv_frame])  # first 180 frames of partial utterance
+                    utterances_spec.append(S[:, -hp.data.tisv_frame:])  # last 180 frames of partial utterance
+
+        utterances_spec = np.array(utterances_spec)
+        print(utterances_spec.shape)
+        if i < train_speaker_num:  # save spectrogram as numpy file
+            np.save(os.path.join(hp.data.train_path, "speaker%d.npy" % i), utterances_spec)
+        else:
+            np.save(os.path.join(hp.data.test_path, "speaker%d.npy" % (i - train_speaker_num)), utterances_spec)
 
 if __name__ == "__main__":
-    save_spectrogram_tisv()
+    save_spectrogram_voxceleb()
